5e470cd1921c0019146c3e716ed76af3cf6b7d6a
DeD_off <DeD_off@WIN-2UFBHT5DQOG>
2016-10-08 13:45:11 +0400

    Добавьте файлы проекта.

---
+++ NoteBooks/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NoteBooks.Startup))]
+namespace NoteBooks
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
