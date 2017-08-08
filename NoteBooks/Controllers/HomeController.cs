df79361aef8e19a153d09a3ae0fc6b43b127fd94
DeD_off <dmi.workbox@gmail.com>
2017-08-08 22:26:54 +0400

    обратная связь

--- NoteBooks/Controllers/HomeController.cs
+++ NoteBooks/Controllers/HomeController.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using NoteBooks.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
+using System.Net.Mail;
+using System.Net;
 
 namespace NoteBooks.Controllers
 {
@@ -12,5 +16,18 @@ namespace NoteBooks.Controllers
         {
             return View();
         }
+
+        public ActionResult SendMessage(Message message)
+        {
+            var client = new SmtpClient("smtp.gmail.com", 587)
+            {
+                Credentials = new NetworkCredential("dmi.workbox@gmail.com", "28mijczc"),
+                EnableSsl = true
+            };
+            client.Send("dmi.workbox@gmail.com", "mechenite@gmail.com", "Свяжитесь со мной", $"Имя: {message.Name}{Environment.NewLine}Контактная информация: {message.ContactData}{Environment.NewLine}Сообщение: {message.Text}");
+            return Json(new {msg="fff" });
+        }
     }
+
+
 }
---
+++ NoteBooks/Models/Message.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NoteBooks.Models
+{
+    public class Message
+    {
+        public string Name { get; set; }
+        public string ContactData { get; set; }
+        public string Text { get; set; }
+    }
+}
